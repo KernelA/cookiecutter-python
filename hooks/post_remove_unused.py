1764ff0d35f2c117711f9cc5d9e87bb9b62e497e
Alexander Kryuchkov <17554646+KernelA@users.noreply.github.com>
2021-09-27 17:56:35 +0300

    Update post_remove_unused.py

--- hooks/post_remove_unused.py
+++ hooks/post_remove_unused.py
@@ -6,5 +6,5 @@ if "{{ cookiecutter.as_package }}" == "no":
     for file in FILES_TO_REMOVE:
         os.remove(file)
 
-if "{{ remove_test_script }}" == "yes":
+if "{{ cookiecutter.remove_test_script }}" == "yes":
     os.remove("test_log.py")
